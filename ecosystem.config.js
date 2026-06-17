ac21899813425a6e9ed7a3b76ec41012ef57c1f7
Neo Luk <neoluk555@hotmail.com>
2026-06-17 20:40:43 +0800

    Revert webhook, keep it simple

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,10 +3,6 @@ module.exports = {
     name: 'eat-what-tmr',
     script: 'server/index.js',
     cwd: __dirname,
-    // 收到 webhook 後 process.exit(0)，pm2 自動重啟
     autorestart: true,
-    // 檔案變更也自動重啟（備用機制）
-    watch: ['output'],
-    watch_delay: 3000,
   }]
 };
